b61343367835f70086e75474c8be0f284fa5bbfa
SiarheiHa <belposchta@gmail.com>
2022-10-01 14:41:13 +0300

    refactor: change productCard to function component

--- react-course/src/components/productCard/productCard.tsx
+++ react-course/src/components/productCard/productCard.tsx
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Product } from 'data/data';
 
 import './productCard.scss';
 import favoriteIcon from './favoriteIcon.svg';
-
-type ProductCardState = {
-  favorite: boolean;
-  inCart: boolean;
-};
 
 type ProductCardProps = {
   product: Product;
@@ -17,35 +12,31 @@ type ProductCardProps = {
   isInFavorites: boolean;
 };
 
-class ProductCard extends Component<ProductCardProps, ProductCardState> {
-  render() {
-    const { set, images, price, ages, pieces, rating } = this.props.product;
-    return (
-      <div className="product">
-        <img
-          onClick={() => this.props.onClickFavorite(this.props.product)}
-          className={
-            this.props.isInFavorites ? 'favorite-icon favorite-icon_active' : 'favorite-icon'
-          }
-          src={favoriteIcon}
-          alt="favorite icon"
-          width="40"
-        />
-        <p className="product__title">{set}</p>
-        <div className="product__img-wrapper">
-          <img className="product__img" src={images[0]} alt="product image" />
-        </div>
-        <p className="product__price">{`$${price}`}</p>
-        <p className="product__ages">{`age:${ages}`}</p>
-        <p className="product__pieces">{`pieces:${pieces}`}</p>
-        <p className="product__rating">{`rating:${Number(rating).toFixed(2)}`}</p>
-        <button
-          onClick={() => this.props.onAddToCart(this.props.product)}
-          className={this.props.isInCart ? 'button button_active' : 'button'}
-        ></button>
+function ProductCard(props: ProductCardProps) {
+  const { set, images, price, ages, pieces, rating } = props.product;
+  return (
+    <div className="product">
+      <img
+        onClick={() => props.onClickFavorite(props.product)}
+        className={props.isInFavorites ? 'favorite-icon favorite-icon_active' : 'favorite-icon'}
+        src={favoriteIcon}
+        alt="favorite icon"
+        width="40"
+      />
+      <p className="product__title">{set}</p>
+      <div className="product__img-wrapper">
+        <img className="product__img" src={images[0]} alt="product image" />
       </div>
-    );
-  }
+      <p className="product__price">{`$ ${price}`}</p>
+      <p className="product__ages">{`age: ${ages}`}</p>
+      <p className="product__pieces">{`pieces: ${pieces}`}</p>
+      <p className="product__rating">{`rating: ${Number(rating).toFixed(2)}`}</p>
+      <button
+        onClick={() => props.onAddToCart(props.product)}
+        className={props.isInCart ? 'button button_active' : 'button'}
+      ></button>
+    </div>
+  );
 }
 
 export default ProductCard;
