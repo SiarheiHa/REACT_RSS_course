761b5191115211e2caf94ccc1cb85bdf64ea474b
SiarheiHa <belposchta@gmail.com>
2022-10-01 13:13:10 +0300

    feat: add filter by search

--- react-course/src/components/listWithSearch/listWithSearch.tsx
+++ react-course/src/components/listWithSearch/listWithSearch.tsx
@@ -29,12 +29,17 @@ class ListWithSearch extends Component<Record<string, never>, ListWithSearchStat
     this.setState({ searchValue: e.currentTarget.value });
   };
 
+  filterBySearch(products: Product[]) {
+    return products.filter(({ set }) => set.includes(this.state.searchValue));
+  }
+
   render() {
     const { products, searchValue } = this.state;
+    const itemsToDraw = this.filterBySearch(products);
     return (
       <div className="list-with-search">
         <SearchBar onInput={this.onInput} value={searchValue} />
-        <ItemList items={products} />
+        <ItemList items={itemsToDraw} />
       </div>
     );
   }
