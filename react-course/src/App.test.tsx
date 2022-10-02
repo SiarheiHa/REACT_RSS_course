d9c80343caddbcd43adf2926eeb98a5b4a18685b
SiarheiHa <belposchta@gmail.com>
2022-10-03 02:48:38 +0300

    test: add test for App

--- react-course/src/App.test.tsx
+++ react-course/src/App.test.tsx
@@ -1,9 +1,60 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('App renders', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole('link').length).toEqual(3);
+  });
+
+  it('Link to About page works', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const link = screen.getByText(/about/i);
+    userEvent.click(link);
+    expect(screen.getByText('AboutPage')).toBeInTheDocument();
+  });
+
+  it('Link to Main page works', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    let link = screen.getByText(/404/i);
+    userEvent.click(link);
+    link = screen.getByText(/main/i);
+    userEvent.click(link);
+    expect(screen.getByText('MainPage')).toBeInTheDocument();
+  });
+
+  it('Link to NotFoundPage works', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const link = screen.getByText(/404/i);
+    userEvent.click(link);
+    expect(screen.getByText('NotFoundPage')).toBeInTheDocument();
+  });
+
+  it('redirect to 404 works', () => {
+    render(
+      <MemoryRouter initialEntries={['/invalidurl']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('NotFoundPage')).toBeInTheDocument();
+  });
 });
