adf787d651651ff1a7086bd1c36405f091b382c1
SiarheiHa <belposchta@gmail.com>
2022-10-15 13:21:24 +0300

    refactor: refactor test for SearchBar

--- react-course/src/components/searchBar/SearchBar.test.tsx
+++ react-course/src/components/searchBar/SearchBar.test.tsx
@@ -1,18 +1,19 @@
-// import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import React from 'react';
-// import SearchBar from './SearchBar';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import SearchBar from './SearchBar';
 
-// describe('SearchBar', () => {
-//   it('search bar renders', () => {
-//     render(<SearchBar value={''} onInput={() => {}} />);
-//     expect(screen.getByRole('searchbox')).toBeInTheDocument();
-//   });
-//   it('callback works', () => {
-//     const onInput = jest.fn();
-//     render(<SearchBar value={''} onInput={onInput} />);
-//     const input = screen.getByRole('searchbox');
-//     userEvent.type(input, 'text');
-//     expect(onInput).toBeCalledTimes(4);
-//   });
-// });
+describe('SearchBar', () => {
+  it('search bar renders', () => {
+    render(<SearchBar value={''} onSubmit={() => {}} />);
+    expect(screen.getByRole('searchbox')).toBeInTheDocument();
+  });
+  it('callback works', () => {
+    const onSubmit = jest.fn();
+    render(<SearchBar value={''} onSubmit={onSubmit} />);
+    const input = screen.getByRole('searchbox');
+    userEvent.type(input, 'text');
+    userEvent.keyboard('{Enter}');
+    expect(onSubmit).toBeCalledTimes(1);
+  });
+});
