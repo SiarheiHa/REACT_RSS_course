7d3044c85e82770b6080f29902145fb60fd0bca2
SiarheiHa <belposchta@gmail.com>
2022-10-15 16:36:12 +0300

    refactor: refactor test for ItemList

--- react-course/src/components/ItemList/ItemList.test.tsx
+++ react-course/src/components/ItemList/ItemList.test.tsx
@@ -1,69 +1,54 @@
-// import { render, screen } from '@testing-library/react';
-// import React from 'react';
-// import ItemList from './ItemList';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ItemList from './ItemList';
 
-// const testItems = [
-//   {
-//     _id: '1',
-//     set: 'TEST The Razor Crest',
-//     item_id: 752927529275292,
-//     reviews: 134,
-//     rating: '4.605769230769231',
-//     availability: 'Available now',
-//     price: 129,
-//     images: [
-//       'https://www.lego.com/cdn/cs/set/assets/blt7a4292faec34e557/75292.png?fit=bounds&format=png&width=455&height=315&dpr=1',
-//       'https://www.lego.com/cdn/cs/set/assets/blt077af3aa46f9b42b/102620-TOTY-SEAL-Winner.jpg?fit=bounds&format=jpg&quality=80&width=455&height=315&dpr=1',
-//     ],
-//     ages: '10+',
-//     pieces: 1023,
-//     __v: 0,
-//   },
-//   {
-//     _id: '2',
-//     set: 'TEST1 The Razor Crest',
-//     item_id: 752927529275292,
-//     reviews: 134,
-//     rating: '4.605769230769231',
-//     availability: 'Available now',
-//     price: 129,
-//     images: [
-//       'https://www.lego.com/cdn/cs/set/assets/blt7a4292faec34e557/75292.png?fit=bounds&format=png&width=455&height=315&dpr=1',
-//       'https://www.lego.com/cdn/cs/set/assets/blt077af3aa46f9b42b/102620-TOTY-SEAL-Winner.jpg?fit=bounds&format=jpg&quality=80&width=455&height=315&dpr=1',
-//     ],
-//     ages: '10+',
-//     pieces: 1023,
-//     __v: 0,
-//   },
-//   {
-//     _id: '3',
-//     set: 'TEST2 The Razor Crest',
-//     item_id: 752927529275292,
-//     reviews: 134,
-//     rating: '4.605769230769231',
-//     availability: 'Available now',
-//     price: 129,
-//     images: [
-//       'https://www.lego.com/cdn/cs/set/assets/blt7a4292faec34e557/75292.png?fit=bounds&format=png&width=455&height=315&dpr=1',
-//       'https://www.lego.com/cdn/cs/set/assets/blt077af3aa46f9b42b/102620-TOTY-SEAL-Winner.jpg?fit=bounds&format=jpg&quality=80&width=455&height=315&dpr=1',
-//     ],
-//     ages: '10+',
-//     pieces: 1023,
-//     __v: 0,
-//   },
-// ];
+const testItems = [
+  {
+    _id: '5cd99d4bde30eff6ebccfc15',
+    height: '1.06m (3\'6")',
+    race: 'Hobbit',
+    gender: 'Male',
+    birth: '22 September ,TA 2968',
+    spouse: '',
+    death: 'Unknown (Last sighting ,September 29 ,3021,) (,SR 1421,)',
+    realm: '',
+    hair: 'Brown',
+    name: 'Frodo Baggins',
+    wikiUrl: 'http://lotr.wikia.com//wiki/Frodo_Baggins',
+  },
+  {
+    _id: '5cd99d4bde30eff6ebccfea0',
+    height: '',
+    race: 'Maiar',
+    gender: 'Male',
+    birth: 'Before the the Shaping of Arda',
+    spouse: '',
+    death: 'January 253019 ,Battle of the Peak immortal',
+    realm: '',
+    hair: 'Grey, later white',
+    name: 'Gandalf',
+    wikiUrl: 'http://lotr.wikia.com//wiki/Gandalf',
+  },
+  {
+    _id: '5cd99d4bde30eff6ebccfd06',
+    height: 'Tall',
+    race: 'Elf',
+    gender: 'Female',
+    birth: 'YT 1362',
+    spouse: 'Celeborn',
+    death: 'Still alive: Departed over the sea on ,September 29 ,3021',
+    realm: 'Eregion,Lothlórien,Caras Galadhon',
+    hair: 'Golden',
+    name: 'Galadriel',
+    wikiUrl: 'http://lotr.wikia.com//wiki/Galadriel',
+  },
+];
 
-// describe('ItemList', () => {
-//   it('item list renders', () => {
-//     render(
-//       <ItemList
-//         items={testItems}
-//         cart={[]}
-//         favorites={[]}
-//         onAddToCart={jest.fn()}
-//         onClickFavorite={jest.fn()}
-//       />
-//     );
-//     expect(screen.getAllByRole('button').length).toEqual(3);
-//   });
-// });
+describe('ItemList', () => {
+  it('item list renders', () => {
+    render(<ItemList items={testItems} onClick={jest.fn()} />);
+    screen.debug();
+    //expect testItems.length + 2 (library wrapper + ItemList wrapper)
+    expect(screen.getAllByRole('generic').length).toEqual(testItems.length + 2);
+  });
+});
