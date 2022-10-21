e27116ef33233a89df21beb9f94ae6bb0c20d106
SiarheiHa <belposchta@gmail.com>
2022-10-21 13:03:30 +0300

    refactor: rewrite FormContainer

--- react-course/src/components/FormContainer/FormContainer.tsx
+++ react-course/src/components/FormContainer/FormContainer.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import { FormContainerState } from 'types/types';
+import React, { useState } from 'react';
 import CardList from './CardList';
 import Form from './Form';
 
 import './FormContainer.scss';
 
-class FormContainer extends React.Component<Record<string, never>, FormContainerState> {
-  state = {
-    cards: [],
+const FormContainer = () => {
+  const [cards, setCards] = useState<Record<string, string>[]>([]);
+
+  const onFormFill = (data: Record<string, string>) => {
+    setCards([...cards, data]);
   };
 
-  onFormFill = (data: Record<string, string | File>) => {
-    this.setState(({ cards }) => {
-      return { cards: [...cards, data] };
-    });
-  };
-
-  render() {
-    return (
-      <div data-testid="form-container">
-        <Form onFormFill={this.onFormFill} />
-        <CardList cards={this.state.cards} />
-      </div>
-    );
-  }
-}
+  return (
+    <div data-testid="form-container">
+      <Form onFormFill={onFormFill} />
+      <CardList cards={cards as Record<string, string>[]} />
+    </div>
+  );
+};
 
 export default FormContainer;
+
+// class FormContainer extends React.Component<Record<string, never>, FormContainerState> {
+//   state = {
+//     cards: [],
+//   };
+
+//   onFormFill = (data: Record<string, string | File>) => {
+//     this.setState(({ cards }) => {
+//       return { cards: [...cards, data] };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div data-testid="form-container">
+//         <Form onFormFill={this.onFormFill} />
+//         <CardList cards={this.state.cards} />
+//       </div>
+//     );
+//   }
+// }
